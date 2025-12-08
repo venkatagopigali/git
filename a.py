b1c81901b49ca59fef9af52352e731e21dc770f2
GALI VENKATA GOPI <venkatagopigali6@gmail.com>
2025-12-08 09:57:58 +0530

    sorry

--- a.py
+++ a.py
@@ -1,8 +1,5 @@
 print("hello world")
 print('************')
 print("i am second person")
-<<<<<<< HEAD
-print('is this correct')
-=======
 print('ok done')
->>>>>>> 3a6890d6eadc26151a1aedc46872ef542c27c7cc
+print('ha')
